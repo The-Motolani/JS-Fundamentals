e313622dba9a1cd3e2a37e9db7014991f81dd388
Motolani <alebiosumotolani@gmail.com>
2025-09-10 20:36:01 +0100

    updated the file '7-multi_c.js'

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
 const {argv} = require('node:process');
 const words = "C is fun";
 
-if (Number(argv[2]) >= 0 && !isNaN(argv[2])) {
-    for (let i = 0; i < argv[2]; i++) {
-    console.log(words);
+if (isNaN(argv[2])) {
+    console.log("Missing number of occurrences");
     }
-}
 else {  
-console.log("Missing number of occurrences");
+
+for (let i = 0; i < argv[2]; i++) {
+    console.log(words);
+}
 }
     //console.log(argv[2]);
