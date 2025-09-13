bfd490eb49f15339fc4088a87382e9a90b619da6
Motolani <alebiosumotolani@gmail.com>
2025-09-13 16:29:46 +0100

    added validation to file

--- 10-factorial.js
+++ 10-factorial.js
@@ -2,6 +2,7 @@ const { argv } = require("node:process");
 let n = Number(argv[2]);
 
 function factorial(n) {
+  if (isNaN(n) || n < 0 || n === 0 || n === 1) return 1;
   let result = 1;
   for (let i = 1; i <= n; i++) {
     result *= i;
